c6700e9cc0ee4ded0f5a24b9f866e5fd69d0e4ca
Bappy71 <19103076@iubat.edu>
2023-02-22 19:44:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ExpenseItem from './Components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
