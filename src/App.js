bc8ce315803471b4de4c58c2628434722c9fba4a
Bappy71 <19103076@iubat.edu>
2023-02-23 02:32:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,41 @@ import './App.css';
 import ExpenseItem from './Components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: "Toilet Paper",
+      amount: 94.67,
+      date: new Date(2021, 7, 28)
+    },
+    {
+      id: 'e2',
+      title: "New TV",
+      amount: 694.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e3',
+      title: "Car Insurense",
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: "New Dex (Wooden)",
+      amount: 594.67,
+      date: new Date(2021, 2, 28)
+    },
+
+  ];
   return (
     <div className="App">
-      <ExpenseItem></ExpenseItem>
+      <h1 className='title-app'>Expense Tracker</h1>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+
     </div>
   );
 }
