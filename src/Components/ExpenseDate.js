c3409fc5e83925b3b5a2575749fce3bd231e162f
Bappy71 <19103076@iubat.edu>
2023-02-24 03:18:19 +0600

    first commit

--- src/Components/ExpenseDate.js
+++ src/Components/ExpenseDate.js
@@ -5,7 +5,7 @@ const ExpenseDate = (props) => {
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
     return (
-        <div>
+        <div className='expense-date'>
             <div>{month}</div>
             <div>{day}</div>
             <div>{year}</div>
