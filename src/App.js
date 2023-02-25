de618d15af792dde0f3b7600c7fca1afa96dd4ca
Bappy71 <19103076@iubat.edu>
2023-02-26 00:46:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import ExpenseItem from './Components/ExpenseItem';
+import Expenses from './Components/Expenses';
+
 
 
 function App() {
@@ -34,11 +35,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='title-app'>Expense Tracker</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
-
+            <Expenses item = {expenses} />
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='card'>
-            <h1>caed</h1>
+            {props.children}
         </div>
     );
 };
