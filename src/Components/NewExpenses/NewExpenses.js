e465189bf26f845e2616c04ac155c7db23b630e5
Bappy71 <19103076@iubat.edu>
2023-02-27 14:55:46 +0600

    first commit

---
+++ src/Components/NewExpenses/NewExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewExpenses.css';
+
+const NewExpenses = () => {
+    return (
+        <div>
+            <form action=""></form>
+        </div>
+    );
+};
+
+export default NewExpenses;
