ae76a01d94cf516040ef5f446fa57e47524bad86
Bappy71 <19103076@iubat.edu>
2023-07-12 22:46:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-
-import './App.css';
-import Expenses from './Components/Expenses/Expenses';
-import NewExpenses from './Components/NewExpenses/NewExpenses';
-
-
+import "./App.css";
+import Expenses from "./Components/Expenses/Expenses";
+import NewExpenses from "./Components/NewExpenses/NewExpenses";
 
 function App() {
   const expenses = [
     {
-      id: 'e1',
+      id: "e1",
       title: "Toilet Paper",
       amount: 94.67,
-      date: new Date(2021, 7, 22)
+      date: new Date(2021, 7, 22),
     },
     {
-      id: 'e2',
+      id: "e2",
       title: "New TV",
       amount: 694.67,
-      date: new Date(2021, 0, 28)
+      date: new Date(2021, 0, 28),
     },
     {
-      id: 'e3',
+      id: "e3",
       title: "Car Insurense",
       amount: 294.67,
-      date: new Date(2021, 1, 28)
+      date: new Date(2021, 1, 28),
     },
     {
-      id: 'e4',
+      id: "e4",
       title: "New Dex (Wooden)",
       amount: 594.67,
-      date: new Date(2021, 4, 28)
+      date: new Date(2021, 4, 28),
     },
-
   ];
   const addExpenseHandler = (expense) => {
-    console.log('IN app.js');
+    console.log("IN app.js");
     console.log(expense);
-  }
+  };
 
   return (
     <div className="App">
-      <h1 className='title-app'>Expense Tracker</h1>
+      <h1 className="title-app">Expense Tracker</h1>
       <NewExpenses onAddExpenses={addExpenseHandler} />
       <Expenses item={expenses} />
-      
     </div>
   );
 }
